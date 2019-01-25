ca1f3a699729947cc2586ea20cae73e1edae3e8d
VovaParamonov <44196011+VovaParamonov@users.noreply.github.com>
2019-01-25 11:15:00 +0600

    first commit

---
+++ public/dist/server.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var userIdCounter = 0;
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.emit('some event', { for: 'everyone' });
+
+
+io.on('connection', function (socket) {
+
+    io.emit('chat message', "server");
+    userIdCounter++;
+    socket.emit('setId', userIdCounter);
+    console.log('a user connected, id='+ userIdCounter);
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg, userId){
+        console.log('[user | id='+userId+']message: ' + msg);
+
+        io.emit('chat message', msg, userId);
+    });
+
+    socket.broadcast.emit('hi');
+
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
