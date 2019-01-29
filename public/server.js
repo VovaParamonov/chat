9de041c856e3f5c4eaaa57ca5b43a4f6eac4e0b5
VovaParamonov <44196011+VovaParamonov@users.noreply.github.com>
2019-01-29 23:19:24 +0600

    'glup'

--- public/server.js
+++ public/server.js
@@ -1,35 +1,48 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+
+app.use(express.static(__dirname + '/dist'));
+
+var globalDate = new Date;
+var logFileName = `messages_log_[${globalDate.getTime()}].txt`;
+fs.writeFile(logFileName,`История сообщений от [${globalDate.getFullYear()}.${globalDate.getMonth()}.${globalDate.getDate()}  ${globalDate.getHours()}:${globalDate.getMinutes()}]`, function (err, data){});
 
 var userIdCounter = 0;
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/dist/index.html');
+app.get('/', function (req, res) {
+    //res.sendfile(__dirname + '/dist/index.html');
+    //res.send('<h1>Главная страница!</h1>');
 });
 
-io.emit('some event', { for: 'everyone' });
 
 
 io.on('connection', function (socket) {
 
-    io.emit('chat message', "server");
+    //io.emit('chat message', "server","server");
     userIdCounter++;
     socket.emit('setId', userIdCounter);
-    console.log('a user connected, id='+ userIdCounter);
-    socket.on('disconnect', function(){
+    console.log('a user connected, id=' + userIdCounter);
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('chat message', function(msg, userId, userName){
-        console.log('[user | '+userName+']message: ' + msg);
+    socket.on('server write', function (userId, userName) {
+        io.emit('chat write', userId, userName);
+    });
+    socket.on('chat message', function (msg, userId, userName, userColor) {
+        let date = new Date;
+        console.log('[user | ' + userName + ']message: ' + msg);
+        fs.appendFile(logFileName, '\n[user | ' + userName + ']: ' + msg, function (err,dat) {});
 
-        io.emit('chat message', msg, userId, userName);
+        io.emit('chat message', msg, userId, userName, userColor, ''+date.getHours()+':'+date.getMinutes());
     });
 
     socket.broadcast.emit('hi');
 
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import $ from 'jquery';
 import './css/style.css';
 
+
+var socket = io();
+//==========================DOM_variables=================================
+var $input = $('#input');
+var $msgsField = $('.messages_ul');
+var $popupWrapper = $('.popupWrapper');
+
+//==========================variables======================================
+var tagName;
+var userId = 0;
+var userName = 'User';
+var userColor = 'Default';
+
+
+
+//-------------------------socket_funcs------------------------------------
+socket.on('setId', function (id) {
+    userId = id;
+});
+
+socket.on('chat message', function (msg, idOtherUser, nameOtherUser) {
+
+    if (userId == idOtherUser) {
+        tagName = '<li class="selfMessage">';
+    } else {
+        tagName = '<li>';
+    }
+
+    $msgsField.append($(tagName).html('<span class="userName">[' + nameOtherUser + ']:</span> ' + msg));
+});
+//========================================================================
+//--------------------------------------DOM_handles-----------------------
+$('.a_settings').click(function () {
+    $popupWrapper.css({"display": "block"});
+    return false;
+});
+
+$popupWrapper.click(function (event) {
+    if (event.target == this) {
+        $popupWrapper.css({"display": "none"})
+    }
+});
+
+
+$('.settingsForm').submit(function (e) {
+    e.preventDefault();
+    userName = $('.settings__name').val();
+    $popupWrapper.css({"display": "none"})
+});
+
+$('.messageForm').submit(function (e) {
+    e.preventDefault();
+    socket.emit('chat message', $input.val(), userId, userName);
+    $input.val('');
+    return false;
+});
+
+$('.a_Clear').click(function () {
+    $('.messages_ul').html('');
+    return false;
+});
+
+$('.radio-wrapper label').click(function () {
+    alert("Работает");
+});
+//==============================================================================
