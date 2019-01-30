b7b1aa2712b09eaf42d41617803920ee78119365
VovaParamonov <44196011+VovaParamonov@users.noreply.github.com>
2019-01-31 00:02:13 +0600

    'debug'

--- public/server.js
+++ public/server.js
@@ -4,26 +4,20 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 
+let users = [];
+
 app.use(express.static(__dirname + '/dist'));
 
-var globalDate = new Date;
-var logFileName = `messages_log_[${globalDate.getTime()}].txt`;
+let globalDate = new Date;
+let logFileName = `messages_log_[${globalDate.getTime()}].txt`;
 fs.writeFile(logFileName, `История сообщений от [${globalDate.getFullYear()}.${globalDate.getMonth()}.${globalDate.getDate()}  ${globalDate.getHours()}:${globalDate.getMinutes()}]`, function (err, data) {
 });
 
-var userIdCounter = 0;
-
-app.get('/', function (req, res) {
-    //res.sendfile(__dirname + '/dist/index.html');
-    //res.send('<h1>Главная страница!</h1>');
-});
 
 
 io.on('connection', function (socket) {
-
-    userIdCounter++;
-    socket.emit('setId', userIdCounter);
-    console.log('a user connected, id=' + userIdCounter);
+    socket.emit('login', users.length+1);
+    //console.log('a user connected, id=' + userIdCounter);
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
@@ -38,9 +32,58 @@ io.on('connection', function (socket) {
 
         io.emit('chat message', msg, userId, userName, userColor, '' + date.getHours() + ':' + date.getMinutes());
     });
+    socket.on('userLogin', function(type ,userName,userPassword, userColor){
+        let err = false;
+        users.forEach(function(elem){
+            if (elem["name"] === userName) {
+                err = true;
+            }
+        });
+        if (err) {
+            type = "login";
+        } else {
+            type = "Register";
+        }
 
-    socket.broadcast.emit('hi');
-
+        if (type === "Register"){
+            let err = false;
+            users.forEach(function(elem){
+               if (elem["name"] === userName) {
+                   console.log("Попытка регистрации с занятым именем");
+                   //socket.emit("login", users.length);
+                   if (elem["password"] == userPassword){
+                       console.log('Удачно');
+                   } else {
+                       console.log("Неудачно");
+                       err = true;
+                   }
+               }
+            });
+            if (err) return false;
+            let userId = users.length;
+            users.push({"id":userId, "name": userName,"password":userPassword, "color":userColor});
+            socket.emit('set settings', userName, userPassword, userColor);
+            console.log("Регистрация нового пользователя: "+userId + " " + userName + " " +userPassword);
+        } else {
+            let err = false;
+            users.forEach(function(elem){
+                if (elem["name"] === userName) {
+                    console.log("Попытка регистрации с занятым именем");
+                    //socket.emit("login", users.length);
+                    if (elem["password"] == userPassword){
+                        console.log('Удачно');
+                    } else {
+                        console.log("Неудачно");
+                        err = true;
+                    }
+                }
+            });
+            if (!err) {
+                console.log('Авторизация пользователя пользователя: ' + userName);
+                socket.emit("set settings", userName, userPassword, userColor);
+            }
+        }
+    });
 });
 
 http.listen(3000, function () {
